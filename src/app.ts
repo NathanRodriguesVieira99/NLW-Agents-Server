9fe395d1c8d6a66a3f552f9008397c4cbcd5236b
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-09 22:55:33 -0300

    refactor: add health check route on specific folder

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,15 @@ import {
 } from "fastify-type-provider-zod";
 import { questionsRoutes } from "./http/routes/questions/index.ts";
 import { roomsRoutes } from "./http/routes/rooms/index.ts";
+import { healthCheckRoute } from "./http/routes/health/healthCheckRoute.ts";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, { origin: "http://localhost:5173" });
 
-app.setSerializerCompiler(serializerCompiler);
+ app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-app.get("/health", () => {
-  return "OK";
-});
-
-app.register(roomsRoutes);
+app.register(healthCheckRoute);
+ app.register(roomsRoutes);
 app.register(questionsRoutes);
