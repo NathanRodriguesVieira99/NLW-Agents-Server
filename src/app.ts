90a80f54b93f5aeab9b1338e652d27b68bd46da3
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-12 00:22:14 -0300

    feat: reorganize imports and register fastifyMultipart

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,26 @@
 import { fastifyCors } from "@fastify/cors";
+import { fastifyMultipart } from "@fastify/multipart";
 import { fastify } from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import { audioRoutes } from "./http/routes/audio/index.ts";
+import { healthCheckRoute } from "./http/routes/health/healthCheckRoute.ts";
 import { questionsRoutes } from "./http/routes/questions/index.ts";
 import { roomsRoutes } from "./http/routes/rooms/index.ts";
-import { healthCheckRoute } from "./http/routes/health/healthCheckRoute.ts";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.register(fastifyCors, { origin: "http://localhost:5173" });
 
- app.setSerializerCompiler(serializerCompiler);
+app.register(fastifyMultipart);
+
+app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
 app.register(healthCheckRoute);
- app.register(roomsRoutes);
+app.register(roomsRoutes);
 app.register(questionsRoutes);
+app.register(audioRoutes);
